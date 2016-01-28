18ec54d6187e7fe2923cbc9aff3a7deccaba57f7
MamdouhAlShamy <mamdouh.al.shamy@gmail.com>
2016-01-28 22:23:08 +0200

    gridSearch

--- Algorithms/gridSearch.py
+++ Algorithms/gridSearch.py
@@ -44,12 +44,15 @@ def isPatternMatch(startingIndex, pattern, grid):
 def removeOutOfRangeIndexes(indexesOfExpectedStaringPattern, patternDim, gridDim):
 	# print indexesOfExpectedStaringPattern
 	indexesToBeDeleted = []
+
 	for i in xrange(len(indexesOfExpectedStaringPattern)):
-		if indexesOfExpectedStaringPattern[i][0] + patternDim[1] > gridDim[1] \
-		or indexesOfExpectedStaringPattern[i][1] + patternDim[0] > gridDim[0]:
+		if indexesOfExpectedStaringPattern[i][0] + patternDim[0] > gridDim[0] \
+		or indexesOfExpectedStaringPattern[i][1] + patternDim[1] > gridDim[1]:
 			indexesToBeDeleted.append(i)
-	for i in range(len(indexesToBeDeleted)-1, -1,-1):
+	
+	for i in range(len(indexesToBeDeleted)-1, -1, -1):
 		del indexesOfExpectedStaringPattern[indexesToBeDeleted[i]]
+
 	# print indexesOfExpectedStaringPattern
 	return indexesOfExpectedStaringPattern
 
