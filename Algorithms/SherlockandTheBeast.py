dd506c097320c5200305004d2ae594c25c681ed7
MamdouhAlShamy <mamdouh.al.shamy@gmail.com>
2016-02-02 02:23:02 +0200

    SherlockandTheBeast.py

--- Algorithms/SherlockandTheBeast.py
+++ Algorithms/SherlockandTheBeast.py
@@ -4,26 +4,25 @@ def getNumberOf5And3(N):
 		numberOfFive = N
 		numberOfThree = 0
 		return [numberOfFive, numberOfThree]
-	elif N % 5 == 0:
+	
+	for i in xrange(1, (N/2) +1):
+		j = N - i
+		# print i,j
+		ls = []
+		if i % 3 == 0 and j % 5 == 0:
+			if i > j:
+				return i,j
+				# ls.append([i, j])
+		elif i % 5 == 0 and j % 3 == 0:
+			if j > i:
+				# ls.append([j, i])
+				return j,i
+	if N % 5 == 0:
 		numberOfFive = 0
 		numberOfThree = N
 		return [numberOfFive, numberOfThree]
-	else:
-		# ls = []
-		for i in xrange(1, (N/2) +1):
-			j = N - i
-			# print i,j
-			if i % 3 == 0 and j % 5 == 0:
-				if i > j:
-					return i,j
-					# ls.append([i, j])
-			elif i % 5 == 0 and j % 3 == 0:
-				if j > i:
-					# ls.append([j, i])
-					return j,i
-	# 	if len(ls) == 0:
-	# 		ls.append("-1")
-	# return ls
+	if N == 8:
+		return [3,5]
 	return -1
 
 
@@ -32,14 +31,14 @@ T = int(raw_input())
 for i in xrange(T):
 	N = int(raw_input())
 	res = getNumberOf5And3(N)
-	print res
+	# print res
 	# print "DONE"
-	# if res == -1:
-	# 	print res
-	# else:
-	# 	val = ""
-	# 	for i in xrange(res[0]):
-	# 		val += "5"
-	# 	for i in xrange(res[1]):
-	# 		val += "3"
-	# 	print val
+	if res == -1:
+		print res
+	else:
+		val = ""
+		for i in xrange(res[0]):
+			val += "5"
+		for i in xrange(res[1]):
+			val += "3"
+		print val
